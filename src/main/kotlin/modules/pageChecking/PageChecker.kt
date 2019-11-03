0f70be3a81ae4f93e9c398a2b4b605ba9b5c283e
Slava0135 <40753025+Slava0135@users.noreply.github.com>
2019-11-03 20:00:42 +0300

    PageChecked refactor

--- src/main/kotlin/modules/pageChecking/PageChecker.kt
+++ src/main/kotlin/modules/pageChecking/PageChecker.kt
@@ -11,48 +11,45 @@ class PageChecker : modules.Module {
     override val millis = 7 * 60 * 60L
     override val name = "Проверка обновления страницы"
     override var lastCalling = System.currentTimeMillis() + 3 * 60 * 60 * 1000L
+
+    private fun sendGet(address: String): String? {
+        try {
+            val url = URL(address)
+            with(url.openConnection() as HttpURLConnection) {
+                requestMethod = "GET"  // optional default is GET
+                println("\nSent 'GET' request to URL : $url; Response Code : $responseCode")
+                return inputStream.bufferedReader().readText()
+            }
+        } catch (e: Exception) {
+            return null
+        }
+    }
+
+    private fun isUpdated(page: String): Boolean? {
+        val filePath = "/data/savedPages/" + page.replace("/", "_") + ".txt"
+        val path = Paths.get("").toAbsolutePath().toString() + filePath
+        val newPage = sendGet(page)
+        if (newPage == null) {
+            println("Не удается получить доступ к странице $page")
+            return null
+        }
+        return try {
+            if (newPage != File(path).readText()) {
+                File(path).bufferedWriter().use { it.write(newPage) }
+                println("Файл $filePath обновлен")
+                true
+            } else {
+                false
+            }
+        } catch (e: FileNotFoundException) {
+            println("Файл $filePath не найден")
+            null
+        }
+    }
+
     override fun call() {
 
-        fun sendGet(address: String): String? {
-            try {
-                val url = URL(address)
-                with(url.openConnection() as HttpURLConnection) {
-                    requestMethod = "GET"  // optional default is GET
-                    println("\nSent 'GET' request to URL : $url; Response Code : $responseCode")
-                    return inputStream.bufferedReader().readText()
-                }
-            } catch (e: Exception) {
-                return null
-            }
-        }
-
-        fun isUpdated(page: String): Boolean? {
-
-            val filePath = "/data/savedPages/" + page.replace("/", "_") + ".txt"
-            val path = Paths.get("").toAbsolutePath().toString() + filePath
-
-            val newPage = sendGet(page)
-            if (newPage == null) {
-                println("Не удается получить доступ к странице $page")
-                return null
-            }
-
-            return try {
-                if (newPage != File(path).readText()) {
-                    File(path).bufferedWriter().use { it.write(newPage) }
-                    println("Файл $filePath обновлен")
-                    true
-                } else {
-                    false
-                }
-            } catch (e: FileNotFoundException) {
-                println("Файл $filePath не найден")
-                null
-            }
-
-        }
-
-        val pages = listOf("http://sergei-sabonis.ru/Student/20192020/dm2019.htm")
+        val pages = listOf("http://sergei-sabonis.ru/Student/20192020/dm2019.htm") //Можно добавить сюда другие сайты
         for (page in pages) {
             if (isUpdated(page) == true) {
                 println("Страница $page была обновлена") //вывод в ВК, можно добавить время и т.п.
