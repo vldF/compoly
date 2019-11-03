8fba4ac7848021380e84fda107d16e38d6510f8f
Slava0135 <40753025+Slava0135@users.noreply.github.com>
2019-11-03 18:11:39 +0300

    fixed check.kt

--- src/main/kotlin/modules/sabonis/check.kt
+++ src/main/kotlin/modules/sabonis/check.kt
@@ -10,22 +10,33 @@ const val page = "http://sergei-sabonis.ru/Student/20192020/dm2019.htm"
 const val folderPath = """\data\sabonis\"""
 const val fileName = """oldPage.txt"""
 
-fun sendGet(address: String): String {
-    val url = URL(address)
-    with(url.openConnection() as HttpURLConnection) {
-        requestMethod = "GET"  // optional default is GET
-        println("\nSent 'GET' request to URL : $url; Response Code : $responseCode")
-        return inputStream.bufferedReader().readText()
+fun sendGet(address: String): String? {
+    try {
+        val url = URL(address)
+        with(url.openConnection() as HttpURLConnection) {
+            requestMethod = "GET"  // optional default is GET
+            println("\nSent 'GET' request to URL : $url; Response Code : $responseCode")
+            return inputStream.bufferedReader().readText()
+        }
+    } catch (e: FileNotFoundException) {
+        return null
     }
 }
 
 fun isUpdated(): Boolean? {
+
     val path = Paths.get("").toAbsolutePath().toString() + folderPath + fileName
-    println(path)
+
     val newPage = sendGet(page)
+    if (newPage == null) {
+        println("Указанной страницы не существует")
+        return null
+    }
+
     return try {
         if (newPage != File(path).readText()) {
             File(path).bufferedWriter().use { it.write(newPage) }
+            println("Файл $folderPath$fileName обновлен")
             true
         } else {
             false
