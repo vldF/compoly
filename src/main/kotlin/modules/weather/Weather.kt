b00fffa5939c31838f483aeb55e062a739664a00
Slava0135 <40753025+Slava0135@users.noreply.github.com>
2019-11-21 19:27:47 +0300

    tweaking

--- src/main/kotlin/modules/weather/Weather.kt
+++ src/main/kotlin/modules/weather/Weather.kt
@@ -1,9 +1,5 @@
 package modules.weather
 import sendGet
 
-const val server = "http://api.openweathermap.org/data/2.5/weather?id=498817&APPID=141ef67be66f26e2a199e2a98f0f34fd"
-
-
-//fun main() {
-//    println(weatherGet(server))
-//}
+const val key = "141ef67be66f26e2a199e2a98f0f34fd"
+const val address = "http://api.openweathermap.org/data/2.5/weather?id=498817&APPID=$key"
