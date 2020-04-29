eba646085253f10c232a7c1b34d08ccc0d27b38b
Slava0135 <super.novalskiy_0135@inbox.ru>
2020-04-29 12:46:38 +0300

    refactored icsEvents

--- src/main/kotlin/modules/events/icsEvents/IcsEvents.kt
+++ src/main/kotlin/modules/events/icsEvents/IcsEvents.kt
@@ -5,38 +5,48 @@ import chatIds
 import modules.Active
 import modules.events.Event
 import modules.events.Time
+import modules.events.daysUntil
 import java.text.SimpleDateFormat
-import kotlin.math.abs
+import java.util.*
 
 @Active
 class IcsEvents : Event {
 
-    override val name = "Check icsEvents from schedule"
+    override val name = "Checks icsEvents from the calendar"
     override val schedule = listOf(Time(8, 0))
 
     private val formatter = SimpleDateFormat("EEEE, d MMMM yyyy")
+    private val scope = 5 //days
 
     override fun call() {
         val reader = Reader()
-        val currentTime = System.currentTimeMillis()
+        val currentTime = Date()
 
         val currentEvents = mutableListOf<String>()
         val nextEvents = mutableListOf<String>()
 
         for (localEvent in reader.read()) {
             val info = "[${localEvent.category}] ${localEvent.name}:\n${formatter.format(localEvent.dateStart)}\n"
-            // todo: refactor next code
-            (if (abs(currentTime / (1000 * 60 * 60 * 24) - localEvent.dateStart / (1000 * 60 * 60 * 24)) <= 1L) currentEvents else if (localEvent.dateStart - currentTime in 0..5 * 24 * 60 * 60 * 1000) nextEvents else mutableListOf()).add(
-                info
-            )
+            val daysUntil = daysUntil(currentTime, Date(localEvent.dateEnd))
+            if (daysUntil == 0L) {
+                currentEvents.add(info)
+            } else if (daysUntil <= scope) {
+                nextEvents.add(info)
+            }
         }
 
         val msg = StringBuilder("\uD83D\uDDD3Ближайшие/недавние:\n")
-        if (currentEvents.isEmpty()) msg.append("Ничего\n")
-        msg.append(currentEvents.joinToString(separator = "\n"))
-        msg.append("\n\uD83D\uDDD35 дней:\n")
-        if (nextEvents.isEmpty()) msg.append("Пусто\n")
-        msg.append(nextEvents.joinToString(separator = "\n"))
+        if (currentEvents.isEmpty())
+            msg.append("Ничего\n")
+        else
+            msg.append(currentEvents.joinToString(separator = "\n"))
+
+        msg.append("\n\uD83D\uDDD3$scope дней:\n")
+        if (nextEvents.isEmpty())
+            msg.append("Пусто\n")
+        else
+            msg.append(nextEvents.joinToString(separator = "\n"))
+
         Vk().send(msg.toString(), chatIds)
     }
 }
