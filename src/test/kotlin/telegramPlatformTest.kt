9101bb996e2f9d5247540e6462ebdae0e55af045
Максим Алпацкий <alpatskij.mi@edu.spbstu.ru>
2020-08-02 18:51:45 +0300

    TelegramTests clean

--- src/test/kotlin/telegramPlatformTest.kt
+++ src/test/kotlin/telegramPlatformTest.kt
@@ -4,22 +4,7 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class TelegramPlatformTest {
-    private val telegram = TelegramPlatform
-    private var lastUpdateId = 0
-    private val TEST_CHAT_ID: Long = -426117826
-    private val picUrl = "https://www.interfax.ru/ftproot/textphotos/2019/05/17/700gc.jpg"
     @Test
     fun send() {
-        telegram.send("test1", TEST_CHAT_ID)
-        var updates = telegram.getUpdates(lastUpdateId + 1) ?: emptyArray()
-        lastUpdateId = updates.last().update_id
-        Assertions.assertEquals("test1", updates.last().message?.text)
-        Assertions.assertEquals(TEST_CHAT_ID, updates.last().message?.chat?.id)
-
-        telegram.send("test2", TEST_CHAT_ID, listOf(picUrl))
-        updates = telegram.getUpdates(lastUpdateId + 1) ?: emptyArray()
-        lastUpdateId = updates.last().update_id
-        Assertions.assertEquals("test2", updates.last().message?.text)
-        Assertions.assertEquals(TEST_CHAT_ID, updates.last().message?.chat?.id)
     }
 }
