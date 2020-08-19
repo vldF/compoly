06296a85846899ef39c1b8dccd8b609d99a49a65
vldf <vladislavf7@gmail.com>
2020-08-19 19:59:17 +0300

    Refactor for description of CapitalismGame

--- src/main/kotlin/modules/chatbot/chatModules/CapitalismGame.kt
+++ src/main/kotlin/modules/chatbot/chatModules/CapitalismGame.kt
@@ -22,7 +22,7 @@ object CapitalismGame {
     private val chatIds = mutableMapOf<String, Long>()
     private val chatsCooldown = mutableMapOf<Long, Long>()
 
-    @OnCommand(["капитализм", "capitalism"], cost = 20, description = "Выбери правильный ответ или отдай e-баллы побеителю")
+    @OnCommand(["капитализм", "capitalism"], cost = 20, description = "выбери правильный ответ или отдай e-баллы побеителю")
     fun startGame(event: LongPollNewMessageEvent) {
         val currentTime = System.currentTimeMillis() / 1000
         if (event.api !is TelegramPlatform) {
