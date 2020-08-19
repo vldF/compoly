4d26a555a3b784f051aacf47599132e3a52488d2
vldf <vladislavf7@gmail.com>
2020-08-19 09:20:56 +0300

    Add ping command

---
+++ src/main/kotlin/modules/chatbot/chatModules/Ping.kt
@@ -0,0 +1,14 @@
+package modules.chatbot.chatModules
+
+import modules.chatbot.CommandPermission
+import modules.chatbot.ModuleObject
+import modules.chatbot.OnCommand
+import modules.chatbot.chatBotEvents.LongPollNewMessageEvent
+
+@ModuleObject
+object Ping {
+    @OnCommand(["ping", "пинг"], "Pong!", CommandPermission.USER)
+    fun ping(event: LongPollNewMessageEvent) {
+        event.api.send("Pong!", event.chatId)
+    }
+}
