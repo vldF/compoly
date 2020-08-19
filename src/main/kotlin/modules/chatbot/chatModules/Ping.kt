d11f38ad6330b09197a75741c82a875d37d38ce7
vldf <vladislavf7@gmail.com>
2020-08-19 19:52:15 +0300

    Admin permissions required

--- src/main/kotlin/modules/chatbot/chatModules/Ping.kt
+++ src/main/kotlin/modules/chatbot/chatModules/Ping.kt
@@ -7,7 +7,7 @@ import modules.chatbot.chatBotEvents.LongPollNewMessageEvent
 
 @ModuleObject
 object Ping {
-    @OnCommand(["ping", "пинг"], "Pong!", CommandPermission.USER)
+    @OnCommand(["ping", "пинг"], "Pong!", CommandPermission.ADMIN)
     fun ping(event: LongPollNewMessageEvent) {
         event.api.send("Pong!", event.chatId)
     }
