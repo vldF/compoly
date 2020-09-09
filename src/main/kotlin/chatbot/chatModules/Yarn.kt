ea8f8782d7a45cb0e36c10381a616a417057ff65
Maxim <alpatskij.mi@edu.spbstu.ru>
2020-09-09 10:02:37 +0300

    yarn probability

--- src/main/kotlin/chatbot/chatModules/Yarn.kt
+++ src/main/kotlin/chatbot/chatModules/Yarn.kt
@@ -6,17 +6,23 @@ import chatbot.chatBotEvents.LongPollNewMessageEvent
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 @ExperimentalStdlibApi
 @ModuleObject
 object Yarn {
+    var probability = 0
     @OnCommand(["нить", "yarn"], cost = 10, description = "Да найдите же ее кто-нибудь")
     fun loseYarn(event: LongPollNewMessageEvent) {
-        event.api.send("произвожу поиск...", event.chatId)
+        event.api.send("Произвожу поиск...", event.chatId)
+        probability++
         val delay = 3000L
         GlobalScope.launch {
             delay(delay)
-            event.api.send("нить потеряна", event.chatId)
+            val found = Random.nextInt(0, 10_000)
+            if (found <= probability)
+                event.api.send("Товарищ, вы нашли нить! Этот день войдёт в историю.", event.chatId)
+            else event.api.send("Нить потеряна", event.chatId)
         }
     }
 }
