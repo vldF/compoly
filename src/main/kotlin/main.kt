20e012bbcc0a6530db800ea0f2cb882b718d30d8
vldF <vladislavF7@gmail.com>
2020-09-18 13:55:48 +0300

    Updated calendar files

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,8 +5,8 @@ import modules.loops.LoopStream
 import org.jetbrains.exposed.sql.Database
 
 fun main() {
-    //EventStream.run()
-    //LoopStream.run()
+    EventStream.run()
+    LoopStream.run()
 
     //initializing db
     Database.connect(hikari())
