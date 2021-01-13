86a17ecb7a538af8c2c25cb1649bbef9610316fe
vldf <vladislavf7@gmail.com>
2021-01-13 19:45:26 +0300

    Add test data

---
+++ src/test/kotlin/testData/cats/userscore.sql
@@ -0,0 +1,4 @@
+-- user_id, chat_id, score, reputation, history_respects, history_disrespects
+insert into UserScore (user_id, chat_id, score) values (1, 1, 100);
+insert into UserScore (user_id, chat_id, score) values (2, 1, 100);
+insert into UserScore (user_id, chat_id, score) values (3, 1, 100);
