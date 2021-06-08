a5e0034e7c25f6723e552b8f619d3ad0e9fba0a2
vldf <vladislavf7@gmail.com>
2021-06-08 14:32:07 +0300

    Fix tests

--- src/test/kotlin/testData/reward/userscore.sql
+++ src/test/kotlin/testData/reward/userscore.sql
@@ -1,12 +1,12 @@
 -- user_id, chat_id, reputation, history_respects, history_disrespects
-insert into UserScore (user_id, chat_id, reputation, history_respects, history_disrespects) values (1, 1, 0, '', '');
-insert into UserScore (user_id, chat_id, reputation, history_respects, history_disrespects) values (2, 1, 5, '', '');
-insert into UserScore (user_id, chat_id, reputation, history_respects, history_disrespects) values (3, 1, 0, '', '');
-insert into UserScore (user_id, chat_id, reputation, history_respects, history_disrespects) values (4, 1, 0, '', '');
-insert into UserScore (user_id, chat_id, reputation, history_respects, history_disrespects) values (5, 1, 0, '', '');
-insert into UserScore (user_id, chat_id, reputation, history_respects, history_disrespects) values (6, 1, 0, '', '');
-insert into UserScore (user_id, chat_id, reputation, history_respects, history_disrespects) values (7, 1, 0, '', '');
-insert into UserScore (user_id, chat_id, reputation, history_respects, history_disrespects) values (8, 1, 0, '', '');
-insert into UserScore (user_id, chat_id, reputation, history_respects, history_disrespects) values (9, 1, 0, '', '');
-insert into UserScore (user_id, chat_id, reputation, history_respects, history_disrespects) values (10, 1, 0, '', '');
-insert into UserScore (user_id, chat_id, reputation, history_respects, history_disrespects) values (11, 1, 0, '', '');
+insert into UserScore (user_id, chat_id, score, history_respects, history_disrespects) values (1, 1, 0, '', '');
+insert into UserScore (user_id, chat_id, score, history_respects, history_disrespects) values (2, 1, 5, '', '');
+insert into UserScore (user_id, chat_id, score, history_respects, history_disrespects) values (3, 1, 0, '', '');
+insert into UserScore (user_id, chat_id, score, history_respects, history_disrespects) values (4, 1, 0, '', '');
+insert into UserScore (user_id, chat_id, score, history_respects, history_disrespects) values (5, 1, 0, '', '');
+insert into UserScore (user_id, chat_id, score, history_respects, history_disrespects) values (6, 1, 0, '', '');
+insert into UserScore (user_id, chat_id, score, history_respects, history_disrespects) values (7, 1, 0, '', '');
+insert into UserScore (user_id, chat_id, score, history_respects, history_disrespects) values (8, 1, 0, '', '');
+insert into UserScore (user_id, chat_id, score, history_respects, history_disrespects) values (9, 1, 0, '', '');
+insert into UserScore (user_id, chat_id, score, history_respects, history_disrespects) values (10, 1, 0, '', '');
+insert into UserScore (user_id, chat_id, score, history_respects, history_disrespects) values (11, 1, 0, '', '');
