53eb65da37717cd88f7f51650b197ab8cac2f2ed
slava0135 <super.novalskiy_0135@inbox.ru>
2021-06-17 13:19:32 +0300

    now

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 echo "${SSH_KEY}" | base64 --decode >/tmp/ssh_rsa
 
-sftp ${SSH_USER}@${SERVER_IP} ${JAR_PATH}
-ssh ${SSH_USER}@${SERVER_IP} ${REMOTE_SCRIPT}
+sftp -i /tmp/ssh_rsa ${SSH_USER}@${SERVER_IP} ${JAR_PATH}
+ssh -i /tmp/ssh_rsa ${SSH_USER}@${SERVER_IP} ${REMOTE_SCRIPT}
