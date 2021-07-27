589e25759e25872fe523b6be77c8188755a8bdcb
vldf <vladislavf7@gmail.com>
2021-07-27 20:34:06 +0300

    Fix travis

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+sudo apt-get install openssh-client
+openssl aes-256-cbc -K $encrypted_b4f3819162af_key -iv $encrypted_b4f3819162af_iv -in travis_key.enc -out ~/travis_key -d
 eval "$(ssh-agent -s)"
 chmod 600 ~/travis_key
 echo -e "Host ${SERVER_IP}\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
