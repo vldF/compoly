ceb9d46094baf471d4fda4a5d45de9a09814564d
vldf <vladislavf7@gmail.com>
2022-03-12 00:48:24 +0300

    Fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 sudo apt-get install openssh-client
 eval "$(ssh-agent -s)"
-chmod 600 ~/deploy_key
+chmod 600 ./deploy_key
 echo -e "Host ${SERVER_IP}\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-ssh-add ~/deploy_key
-sudo scp -o StrictHostKeyChecking=no -i ~/deploy_key ./build/libs/vkBot.jar ubuntu@"${SERVER_IP}":/home/vldf/vkBot/lib/vkBot.jar
-sudo ssh -o StrictHostKeyChecking=no -i ~/deploy_key vldf@"${SERVER_IP}" sh /home/vldf/vkBot/restart.sh
+ssh-add ./deploy_key
+sudo scp -o StrictHostKeyChecking=no -i ./deploy_key ./build/libs/vkBot.jar ubuntu@"${SERVER_IP}":/home/vldf/vkBot/lib/vkBot.jar
+sudo ssh -o StrictHostKeyChecking=no -i ./deploy_key vldf@"${SERVER_IP}" sh /home/vldf/vkBot/restart.sh
