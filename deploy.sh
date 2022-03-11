d7d413ffadd04b455ed15b1d135ebc0b20241345
vldf <vladislavf7@gmail.com>
2022-03-11 20:19:42 +0000

    new deploy server

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 sudo apt-get install openssh-client
-openssl aes-256-cbc -K $encrypted_b4f3819162af_key -iv $encrypted_b4f3819162af_iv -in travis_key.enc -out ~/travis_key -d
 eval "$(ssh-agent -s)"
-chmod 600 ~/travis_key
+chmod 600 ~/deploy_key
 echo -e "Host ${SERVER_IP}\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-ssh-add ~/travis_key
-sudo scp -o StrictHostKeyChecking=no -i ~/travis_key ./build/libs/vkBot.jar ubuntu@"${SERVER_IP}":/home/ubuntu/vkBot/lib/vkBot.jar
-sudo ssh -o StrictHostKeyChecking=no -i ~/travis_key ubuntu@"${SERVER_IP}" sh /home/ubuntu/restartbot.sh
+ssh-add ~/deploy_key
+sudo scp -o StrictHostKeyChecking=no -i ~/deploy_key ./build/libs/vkBot.jar ubuntu@"${SERVER_IP}":/home/vldf/vkBot/lib/vkBot.jar
+sudo ssh -o StrictHostKeyChecking=no -i ~/deploy_key ubuntu@"${SERVER_IP}" sh /home/vldf/restartbot.sh
